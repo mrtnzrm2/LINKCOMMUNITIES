902da18b359dbd57a75684cfae7f828ed3d7073c
Jorge Martinez <csg1c12@gmail.com>
2022-03-13 16:56:25 -0400

    Try to make it work for the case that the distance between link communities is only done by connected edges, omitting unconnected edges. However, it didn't work since at the beginning, most of the edges are unconnected and they get a zero distance.

--- cpp/custom_hierarchical_cluster.cpp
+++ cpp/custom_hierarchical_cluster.cpp
@@ -7,6 +7,7 @@
 #include <math.h>
 #include <stdio.h>
 #include <limits.h>
+#include <numeric>
 using namespace std::chrono;
 using namespace std;
 
@@ -35,25 +36,41 @@ vector<vector<long double> > read_csv(const string fname){
 	return content;
 }
 
+template <typename T>
+T average(vector<T> const& v){
+    if(v.empty()){
+        return 0;
+    }
+
+    auto const count = static_cast<T>(v.size());
+    return std::reduce(v.begin(), v.end()) / count;
+}
+
 template <typename T, typename I>
 void custom_dis_matrix(vector<vector<T> > &matrix, const I &N){
     
+    vector<T> average_dis;
+    T ave;
     T max_dis = -1;
 
     for (int i=0; i < N-1; i++){
         for (int j=0; j < N-i-1; j++){
             if (matrix[i][j] > 0){
                 matrix[i][j] = -log10(matrix[i][j]);
+                average_dis.push_back(matrix[i][j]);
                 if (max_dis < matrix[i][j])
                     max_dis = matrix[i][j];
             }
         }
     }
 
+    ave = average(average_dis);
+
     for (int i=0; i < N-1; i++){
         for (int j=0; j < N-i-1; j++){
             if (matrix[i][j] <= 0)
-                matrix[i][j] = max_dis + 1;
+                matrix[i][j] = ave;
+                // matrix[i][j] = max_dis + 1;
         }
     }
 }
@@ -153,22 +170,143 @@ T single_linkage(vector<I> &m1, vector<I> &m2, vector<vector<T> >  &dist){
 long double average_linkage(vector<int_fast32_t> &m1, vector<int_fast32_t> &m2, vector<vector<long double> >  &dist){
 
     long double dist1 = 0;
+    long double tmp;
+    int_fast32_t count=0;
+
     for (int i=0; i < m1.size(); i++){
         for (int j=0; j < m2.size(); j++){
 
             if (m1[i] == m2[j]){
                 cout << "The same node is in two different clusters\n";
             } else if (m1[i] > m2[j]){
-                dist1 += dist[m2[j]][m1[i]-m2[j]-1];
+                tmp = dist[m2[j]][m1[i]-m2[j]-1];
+                if (tmp > 0){
+                    dist1 += tmp;
+                    count++;
+                }
             } else{
-                dist1 += dist[m1[i]][m2[j]-m1[i]-1];
+                tmp = dist[m1[i]][m2[j]-m1[i]-1];
+                if (tmp > 0){
+                    dist1 += tmp;
+                    count++;
+                }
             }
         }
     }
 
-    return dist1/(m1.size()*m2.size());
+    if (count > 0){
+        dist1 /= count;
+        // cout << "Warning: link communities are totally unconneted\n ";
+        // cout << dist1 << endl;
+    }
+
+    return dist1;
 }
 
+template<typename T, typename I>
+hclust<T,I> hierarchical_clustering(vector<vector<T> >  &dist, const I &steps, const I &th){
+    hclust<T,I> H;
+    H.merge = vector<vector<I> > (steps-1,vector<I>(2,0));
+    H.height = vector<T> (steps-1,0);
+    vector<I> inter;
+    T min_dist;
+    T dist_g1;
+    T dist_g2;
+    I N;
+    I u;
+    I v;
+
+    vector<nodes<I> > hierarchy(steps);
+    for (int i=0; i < steps; i++){
+        hierarchy[i].root = -(i+1);
+        hierarchy[i].members.push_back(i);
+    }
+
+    for (int e=0; e < steps-1; e++){
+
+        if ((int) 100*e/(steps-1)%10 == 0 && (int) 100*e/(steps-1) >= 1)
+            cout << (int) 100*e/(steps-1)<< '%' << endl;
+
+        min_dist = INT_MAX;
+        N = hierarchy.size();
+        if (N <= 1)
+            continue;
+        for (int x=0; x < N-1; x++){
+            for (int y=0; y <= (N-x-1)/2; y++){
+                if (e < th){
+                    dist_g1 = single_linkage(hierarchy[x].members, hierarchy[y+x+1].members, dist);
+                    dist_g2 = single_linkage(hierarchy[x].members, hierarchy[N-y-1].members, dist);
+                } else{
+                    dist_g1 = average_linkage(hierarchy[x].members, hierarchy[y+x+1].members, dist);
+                    dist_g2 = average_linkage(hierarchy[x].members, hierarchy[N-y-1].members, dist);
+                }
+                if (dist_g1 < dist_g2){
+                    if(min_dist > dist_g1){
+                        min_dist = dist_g1;
+                        u = x;
+                        v = y+x+1;
+                    }
+                } else{
+                    if(min_dist > dist_g2){
+                        min_dist = dist_g2;
+                        u = x;
+                        v = N-y-1;
+                    }
+                }
+            }
+        }
+
+        H.height[e] = min_dist/2;
+        H.merge[e][0] = hierarchy[u].root;
+        H.merge[e][1] = hierarchy[v].root;
+        hierarchy[u].root = e+1;
+        inter.clear();
+        inter.reserve(hierarchy[u].members.size() + hierarchy[v].members.size());
+        inter.insert(inter.end(), hierarchy[u].members.begin(), hierarchy[u].members.end());
+        inter.insert(inter.end(), hierarchy[v].members.begin(), hierarchy[v].members.end());
+        hierarchy[u].members = inter;
+        hierarchy.erase(hierarchy.begin()+v);
+    }
+
+    return H;
+}
+
+template <typename S>
+ostream& operator<<(ostream &os, const vector<S> &vector){
+    // Printing all the elements
+    // using <<
+    for (auto element : vector) {
+        os << element << " ";
+    }
+    return os;
+}
+
+template <typename I>
+void out_csv_2d(vector<vector<I> > &matrix, const string &out_path){
+
+    I sx = matrix.size();
+    I sy = matrix[0].size();
+	ofstream out(out_path);
+	for (int i=0; i < sx; i++){
+		for (int j=0; j < sy; j++){
+			if (j < sy -1)
+				out << matrix[i][j] << ',';
+			else
+				out << matrix[i][j];
+		}
+		out << '\n';
+	}
+}
+
+template <typename T>
+void out_csv_1d(vector<T> &matrix, const string &out_path){
+	ofstream out(out_path);
+	for (int i=0; i < matrix.size(); i++){
+		out << matrix[i] << '\n';
+	}
+}
+
+//// Paralelization project: still needs work
 template<typename I>
 vector<vector<int_fast32_t> > nodes_parallelization(I &N, const I &chunks){
 
@@ -312,135 +450,47 @@ hclust<T,I> hierarchical_clustering_parallel(vector<vector<T> >  &dist, const I 
 
     return H;
 }
-
-template<typename T, typename I>
-hclust<T,I> hierarchical_clustering(vector<vector<T> >  &dist, const I &steps, const I &th){
-    hclust<T,I> H;
-    H.merge = vector<vector<I> > (steps-1,vector<I>(2,0));
-    H.height = vector<T> (steps-1,0);
-    vector<I> inter;
-    T min_dist;
-    T dist_g1;
-    T dist_g2;
-    I N;
-    I u;
-    I v;
-
-    vector<nodes<I> > hierarchy(steps);
-    for (int i=0; i < steps; i++){
-        hierarchy[i].root = -(i+1);
-        hierarchy[i].members.push_back(i);
-    }
-
-    for (int e=0; e < steps-1; e++){
-
-        if ((int) 100*e/(steps-1)%10 == 0 && (int) 100*e/(steps-1) >= 1)
-            cout << (int) 100*e/(steps-1)<< '%' << endl;
-
-        min_dist = INT_MAX;
-        N = hierarchy.size();
-        if (N <= 1)
-            continue;
-        for (int x=0; x < N-1; x++){
-            for (int y=0; y <= (N-x-1)/2; y++){
-                if (e < th){
-                    dist_g1 = single_linkage(hierarchy[x].members, hierarchy[y+x+1].members, dist);
-                    dist_g2 = single_linkage(hierarchy[x].members, hierarchy[N-y-1].members, dist);
-                } else{
-                    dist_g1 = average_linkage(hierarchy[x].members, hierarchy[y+x+1].members, dist);
-                    dist_g2 = average_linkage(hierarchy[x].members, hierarchy[N-y-1].members, dist);
-                }
-                if (dist_g1 < dist_g2){
-                    if(min_dist > dist_g1){
-                        min_dist = dist_g1;
-                        u = x;
-                        v = y+x+1;
-                    }
-                } else{
-                    if(min_dist > dist_g2){
-                        min_dist = dist_g2;
-                        u = x;
-                        v = N-y-1;
-                    }
-                }
-            }
-        }
-
-        H.height[e] = min_dist/2;
-        H.merge[e][0] = hierarchy[u].root;
-        H.merge[e][1] = hierarchy[v].root;
-        hierarchy[u].root = e+1;
-        inter.clear();
-        inter.reserve(hierarchy[u].members.size() + hierarchy[v].members.size());
-        inter.insert(inter.end(), hierarchy[u].members.begin(), hierarchy[u].members.end());
-        inter.insert(inter.end(), hierarchy[v].members.begin(), hierarchy[v].members.end());
-        hierarchy[u].members = inter;
-        hierarchy.erase(hierarchy.begin()+v);
-    }
-
-    return H;
-}
-
-template <typename S>
-ostream& operator<<(ostream &os, const vector<S> &vector){
-    // Printing all the elements
-    // using <<
-    for (auto element : vector) {
-        os << element << " ";
-    }
-    return os;
-}
-
-template <typename I>
-void out_csv_2d(vector<vector<I> > &matrix, const string &out_path){
-
-    I sx = matrix.size();
-    I sy = matrix[0].size();
-	ofstream out(out_path);
-	for (int i=0; i < sx; i++){
-		for (int j=0; j < sy; j++){
-			if (j < sy -1)
-				out << matrix[i][j] << ',';
-			else
-				out << matrix[i][j];
-		}
-		out << '\n';
-	}
-}
-
-template <typename T>
-void out_csv_1d(vector<T> &matrix, const string &out_path){
-	ofstream out(out_path);
-	for (int i=0; i < matrix.size(); i++){
-		out << matrix[i] << '\n';
-	}
-}
+////
 
 int main(){
 
+    //// Create variables
+    //strings
     string out_path;
-    const string csv_path = "../CSV/91x40/sim.csv"; // merged/similarity/tracto2016/zz_model/
-	const int_fast32_t leaves = 999; // 6868
-    const int_fast32_t threshold = leaves - 181; //
-    const int_fast32_t chunks = 6;
+    const string csv_path = "../CSV/merged/similarity/tracto2016/zz_model/sim_l10.csv"; // merged/similarity/tracto2016/zz_model/
+    //ints
+	const int_fast32_t leaves = 6868; // 6868
+    const int_fast32_t threshold = 0; //
+
+    //// Start code
+    cout << "Read similarity matrix\n";
     vector<vector<long double> > distance = read_csv(csv_path);
+    cout << "Finished";
+    printf("leaves: %i   threshold: %i\n", leaves, threshold);
 
+    cout << "Customized similarity\n";
     custom_dis_matrix(distance, leaves);
+    cout << "Finished\n";
 
+    cout << "START CUSTOMIZED HIERARCHICAL ALGORITHM\n";
     auto start = high_resolution_clock::now();
     hclust<long double, int_fast32_t> hyr = hierarchical_clustering(distance, leaves, threshold);
     auto stop = high_resolution_clock::now();
+    cout << "Finshed";
+
 
     auto duration = duration_cast<microseconds>(stop - start);
-    cout << duration.count() << endl;
+    cout << "Time: " << duration.count() << endl;
     
     vector<vector<int_fast32_t> > merge = hyr.merge;
     vector<long double> height = hyr.height;
 
-    out_path = "../CSV/91x40/merge_th_181.csv";
+    cout << "Saving the height and merge vectors\n";
+    out_path = "../CSV/merged/hclust/tracto2016/zz_model/TH_0_INDEPENDENT/merge_.csv";
     out_csv_2d(merge, out_path);
-    out_path = "../CSV/91x40/height_th_181.csv";
+    out_path = "../CSV/merged/hclust/tracto2016/zz_model/TH_0_INDEPENDENT/height.csv";
     out_csv_1d(height, out_path);
+    cout << "End";
 
     return 0;
 }
