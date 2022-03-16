ddc12525836c7fe3bcb2e6c02008e25f72aaa03a
Jorge Martinez <csg1c12@gmail.com>
2022-03-16 09:27:24 -0400

    Make it work without needing to fit the model inside the function but taking the model fitted as an argument

--- R/functions/save_work_experiment.R
+++ R/functions/save_work_experiment.R
@@ -1,51 +1,80 @@
-save.work.experiment <- function(model, datasets, ids, serie, inst, mats, suffix=""){
+save.work.experiment <- function(
+  model, datasets, ids, inst, mats,
+  serie=0, trial=0, fold=0) {
   source("functions/eval_tools.R")
-  preprocessing.recipe <- recipes::recipe(w ~ ., rsample::training(datasets)) %>% recipes::prep()
-  xgboost.model <- model$model %>%
-    tune::finalize_model(model$parameters)
-  # Compute in train ----
-  train.processed <- recipes::bake(preprocessing.recipe,  new_data = rsample::training(datasets))
-  train.model <- xgboost.model %>%
-    # fit the model on all the training data
-    parsnip::fit(
-      formula = w ~ ., 
-      data    = train.processed
-    ) 
-  best.score <- xgboost::xgb.importance(model = train.model$fit) %>% dplyr::as_tibble()
-  train.prediction <- train.model %>%
-    predict(new_data = train.processed) %>%
-    dplyr::bind_cols(rsample::training(datasets))
-  # Compute in test ----
-  test.processed <- recipes::bake(preprocessing.recipe,  new_data = rsample::testing(datasets))
-  test.prediction <- train.model %>%
-    predict(new_data = test.processed) %>%
-    dplyr::bind_cols(rsample::testing(datasets))
+  train_model <- model$train
+  test_model <- model$test
+  best_score <- xgboost::xgb.importance(
+            model = train_model$fit) %>%
+            dplyr::as_tibble()
+  test_prediction <- test_model %>%
+    tune::collect_predictions() %>%
+    dplyr::group_by(id)
+  train_rmae <- train_model %>%
+    tune::collect_metrics()
+  train_rmae <- train_rmae$mean[1]
   # Ensemble parameters ----
   source("functions/rmae.R")
-  parameters <- model$parameters %>% 
+  parameters <- model$parameters %>%
     dplyr::bind_cols(
       dplyr::tibble(
-        train.rmae = rmae(train.prediction$w, train.prediction$.pred),
-        test.rmae = rmae(test.prediction$w %>% get.nonzero(mats$test), test.prediction$.pred %>% get.nonzero(mats$test)),
-        norm.trarin.rmae = rmae(train.prediction$w, norm.pred.train(train.prediction$.pred, mats$train, ids$train)),
-        norm.test.rmae = rmae(test.prediction$w %>% get.nonzero(mats$test), norm.pred.test(test.prediction$.pred, mats$test, ids$test))
-      ), best.score
+        train.rmae = train_rmae,
+        rmae(rsample::testing(datasets)$w, test_prediction$.pred),
+      ), best_score
     )
   # Ensemble regression
-  regression.values <- dplyr::tibble(w=rsample::testing(datasets)$w %>% get.nonzero(mats$test), pred=test.prediction$.pred %>% get.nonzero(mats$test), norm.pred=norm.pred.test(test.prediction$.pred, mats$test, ids$test), id=ids$test, serie=paste(serie, suffix, sep = "_"))
+  regression_values <- dplyr::tibble(
+    w = rsample::testing(datasets)$w %>%
+     get.nonzero(mats$test),
+    pred = test.prediction$.pred %>%
+      get.nonzero(mats$test),
+    id = ids$test, serie = serie, trial = trial, fold = fold
+  )
   # Check if files exist ----
-  if (!file.exists("../CSV/%s/XGBOOST/%s/model_predictions_%i.csv" %>% sprintf(inst$folder, inst$common, serie)))
-    write.csv(regression.values, "../CSV/%s/XGBOOST/%s/model_predictions_%i.csv" %>% sprintf(inst$folder, inst$common, serie), row.names = F)
+  if (!file.exists(
+        "../CSV/%s/XGBOOST/%s/model_predictions_%i.csv" %>%
+          sprintf(inst$folder, inst$common, serie)
+      )
+  )
+    write.csv(
+      regression_values,
+      "../CSV/%s/XGBOOST/%s/model_predictions_%i.csv" %>%
+        sprintf(inst$folder, inst$common, serie), row.names = F
+    )
   else{
-    r <- read.csv("../CSV/%s/XGBOOST/%s/model_predictions_%i.csv" %>% sprintf(inst$folder, inst$common, serie))
-    regression.values <- r %>% dplyr::bind_rows(regression.values)
-    write.csv(regression.values, "../CSV/%s/XGBOOST/%s/model_predictions_%i.csv" %>% sprintf(inst$folder, inst$common, serie), row.names = F)
+    r <- read.csv(
+      "../CSV/%s/XGBOOST/%s/model_predictions_%i.csv" %>%
+        sprintf(inst$folder, inst$common, serie)
+    )
+    regression_values <- r %>%
+      dplyr::bind_rows(regression_values)
+    write.csv(
+      regression.values,
+      "../CSV/%s/XGBOOST/%s/model_predictions_%i.csv" %>%
+        sprintf(inst$folder, inst$common, serie), row.names = F
+    )
   }
-  if (!file.exists("../CSV/%s/XGBOOST/%s/xgboost_parameters_%i.csv" %>% sprintf(inst$folder, inst$common, serie)))
-    write.csv(parameters, "../CSV/%s/XGBOOST/%s/xgboost_parameters_%i.csv" %>% sprintf(inst$folder, inst$common, serie), row.names = F)
+  if (!file.exists(
+        "../CSV/%s/XGBOOST/%s/xgboost_parameters_%i.csv" %>%
+          sprintf(inst$folder, inst$common, serie)
+      )
+  )
+    write.csv(
+      parameters,
+      "../CSV/%s/XGBOOST/%s/xgboost_parameters_%i.csv" %>%
+        sprintf(inst$folder, inst$common, serie), row.names = F
+    )
   else{
-    r <- read.csv("../CSV/%s/XGBOOST/%s/xgboost_parameters_%i.csv" %>% sprintf(inst$folder, inst$common, serie))
-    parameters <- r %>% dplyr::bind_rows(parameters)
-    write.csv(parameters, "../CSV/%s/XGBOOST/%s/xgboost_parameters_%i.csv" %>% sprintf(inst$folder, inst$common, serie), row.names = F)
+    r <- read.csv(
+      "../CSV/%s/XGBOOST/%s/xgboost_parameters_%i.csv" %>%
+        sprintf(inst$folder, inst$common, serie)
+    )
+    parameters <- r %>%
+      dplyr::bind_rows(parameters)
+    write.csv(
+      parameters,
+      "../CSV/%s/XGBOOST/%s/xgboost_parameters_%i.csv" %>%
+        sprintf(inst$folder, inst$common, serie), row.names = F
+    )
   }
 }
