5a8689823d6ad2acf1e30f08d7e1ef1da1333d9c
Jorge Martinez <csg1c12@gmail.com>
2022-06-10 13:00:39 -0400

    Toy function to make some graphs and explore tools.

--- R/main_imputation*.R
+++ R/main_imputation*.R
@@ -9,18 +9,19 @@ version <- 'merged'
 dist.ver <- 'tracto2016'
 threshold <- 0.528024
 
-format.path <- list(system = system,
-                    model = model,
-                    variant = variant,
-                    subject = subject,
-                    version = version,
-                    dist.ver = dist.ver)
+format.path <- list(
+  system = system,
+  model = model,
+  variant = variant,
+  subject = subject,
+  version = version,
+  dist.ver = dist.ver
+)
 
 get.path <- function(path, folder){
-  
-  path <- c(path, list(folder=folder))
-  file.path <- '{folder}/{subject}/{system}/{version}/{dist.ver}/{model}/{variant}'
-  
+  path <- c(path, list(folder = folder))
+  file.path <-
+    "{folder}/{subject}/{system}/{version}/{dist.ver}/{model}/{variant}"
   return(sformat(file.path, path))
 }
 
@@ -40,21 +41,47 @@ imputation <- imputation * adjacency
 imputation[imputation == 0 ] <- NA
 
 for (i in 1:nrow(imputation)){
-  imputation[,i] <- imputation[,i]/sum(imputation[,i], na.rm = T)
+  imputation[,i] <- imputation[,i] / sum(imputation[,i], na.rm = T)
 }
 
 source('functions/plot_matrix.R')
-png(file.path('..', 'plots','merged','{dist.ver}/{model}/{variant}' %>% sformat(format.path),'imputation_w.png'), width = 6, height = 5, units = 'in', res = 200)
+png(
+  file.path(
+    '..',
+    'plots',
+    'merged',
+    '{dist.ver}/{model}/{variant}' %>%
+      sformat(format.path),
+    'imputation_w.png'
+  ), width = 6, height = 5, units = "in", res = 200)
 plot.matrix(log10(imputation)+7)
 dev.off()
 
 imputation[is.na(imputation)] <- 0
-write.csv(imputation, file.path('..','CSV','imputation_{model}_{variant}_{dist.ver}.csv' %>% sformat(format.path)), row.names = F)
+write.csv(
+  imputation,
+  file.path(
+    '..',
+    'CSV',
+    'imputation_{model}_{variant}_{dist.ver}.csv' %>%
+      sformat(format.path)
+    ), row.names = F
+  )
 
-for (e in 1:length(fln.col)){
+for (e in 1:length(fln.col)) {
   if(grepl('X',fln.col[e], fixed = T)){
-    col_e <- stringr::str_split(fln.col[e], 'X') %>% unlist()
+    col_e <- stringr::str_split(fln.col[e], 'X') %>%
+      unlist()
     fln.col[e] <- col_e[2]
   }
 }
-write.csv(fln.col, file.path('..','CSV','imputation_{model}_{variant}_{dist.ver}_clabel.csv' %>% sformat(format.path)), row.names = F)
+write.csv(
+  fln.col,
+  file.path(
+    '..',
+    'CSV',
+    'imputation_{model}_{variant}_{dist.ver}_clabel.csv' %>%
+      sformat(format.path)
+    ),
+    row.names = F
+  )
